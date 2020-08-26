60d7dfa3d42eb40bdc3722b137a3e7c459205f5e
John Glennan <glennan20@gmail.com>
2020-08-26 19:19:28 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,6 +15,6 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  contacts["Freddy Mercury"][:favorite_ice_cream_flavors].pop("strawberry")
+  contacts["Freddy Mercury"][:favorite_ice_cream_flavors].shift("strawberry")
   contacts
 end
